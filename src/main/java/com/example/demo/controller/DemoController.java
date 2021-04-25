b37af593d53eeafdfd1d0285adcf7e2b31cdc7af
rakeshranjan1 <rakesh.ranjan1@lenskart.in>
2021-04-25 16:44:53 +0530

    initial commit

--- src/main/java/com/example/demo/controller/DemoController.java
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -7,7 +7,22 @@ import org.springframework.web.bind.annotation.RestController;
 public class DemoController {
 	
 	@GetMapping(path = "/")
-	public String home() {
+	public String base() {
 		return "Welcome to My Demo Spring Boot";
+	}
+	
+	@GetMapping(path = "/home")
+	public String home() {
+		return "Welcome to My home page";
+	}
+	
+	@GetMapping(path = "/about")
+	public String about() {
+		return "Welcome to My About Page";
+	}
+	
+	@GetMapping(path = "/*")
+	public String wrong() {
+		return "Page Not Found";
 	}
 }
